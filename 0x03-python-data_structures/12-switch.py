95fa0d1373064b124805b31b19faaacbe5e96742
Yanda7 <ayandasikhosana99@gmail.com>
2023-12-05 14:09:13 -0800

    new changes

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
-def switch_values(a, b):
-    # This line will be inserted in the code
-    return b, a
-
-a = 10
-b = 89
-
-print("a=10 - b=89")
-
-a, b = switch_values(a, b)
-
-print("a=10 - b=89")
+a = 89
+b = 10
+a, b = b, a
+print("a={:d} - b={:d}".format(a, b))
